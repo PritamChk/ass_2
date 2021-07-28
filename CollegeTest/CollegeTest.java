8e3bde808897bad8ded116048295f5b8748dfb9e
Pritam Chakraborty <pritamchakraborty2017@gmail.com>
2021-07-28 11:35:49 +0530

    prev collegestudent project deleted

--- CollegeTest/CollegeTest.java
+++ CollegeTest/CollegeTest.java
@@ -1,5 +1,14 @@
 package CollegeTest;
 
+import CollegeStudent.college.College;
+
 public class CollegeTest {
-    
+    public static void main(String[] args) {
+        College[] colleges = {
+            College("TMSL","130"),
+            College("SXC","102")
+        }    
+    }
+
+
 }
